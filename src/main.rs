3580439806e48686a89d71b2b80086c356542db3
aryankad1an <aryankaddian842@gmail.com>
2024-06-15 19:18:47 +0530

    reading from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    // we will pass args as cargo run {query} {filename}
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    let contents =
+        std::fs::read_to_string(filename).expect("Something went wrong reading the file");
 }
